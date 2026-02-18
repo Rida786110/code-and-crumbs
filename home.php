c4059eb9ad2d075323ad6407057bb3b647048c0e
Rida <ridazehraaliraza@gmail.com>
2026-02-18 12:36:06 +0500

    Added home content

---
+++ home.php
@@ -0,0 +1,6 @@
+<?php 
+  echo "<section style='text-align:center; padding:50px; background:#f4f4f4;'>
+          <h1>Welcome to Our Homepage</h1>
+          <p>This is a professional WordPress project built with Git branches.</p>
+        </section>";
+?>
